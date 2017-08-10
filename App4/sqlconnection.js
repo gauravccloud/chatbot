67762b8bacd8b076907d0808a0c58caf9be98f37
gauravccloud <gaurav.sharma@cognitiveclouds.com>
2017-08-10 11:59:57 +0530

    SQL OCnnection

--- App4/sqlconnection.js
+++ App4/sqlconnection.js
@@ -2,23 +2,23 @@ var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 
 // Create connection to database
-var config = 
+var config =
    {
      userName: 'jiffybots',
      password: 'abcd1234$',
      server: 'ocbotdata.database.windows.net',
-     options: 
+     options:
         {
-           database: 'OlympediaV2', 
+           database: 'OlympediaV2',
            encrypt: true
         }
    }
 var connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err) 
+connection.on('connect', function(err)
    {
-     if (err) 
+     if (err)
        {
           console.log(err)
        }
@@ -30,26 +30,27 @@ connection.on('connect', function(err)
    }
  );
 
-    function executeStatement() {  
-        request = new Request("SELECT * FROM ocParticipants", function(err) {  
-        if (err) {  
-            console.log(err);}  
-        });  
-        var result = "";  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                result+= column.value + " ";  
-              }  
-            });  
-            console.log(result);  
-            result ="";  
-        });  
+    function executeStatement() {
+        request = new Request("SELECT * FROM athletes", function(err) {
+        if (err) {
+            console.log(err);}
+        });
+        var result = "";
+        request.on('row', function(columns) {
+            //console.log(columns);
+            columns.forEach(function(column) {
+              if (column.value === null) {
+              //  console.log('NULL');
+              } else {
+                result+= column.value + " ";
+              }
+            });
+            //console.log(result);
+            result ="";
+        });
 
-        request.on('done', function(rowCount, more) {  
-        console.log(rowCount + ' rows returned');  
-        });  
-        connection.execSql(request);  
-    }  
+        request.on('done', function(rowCount, more) {
+        console.log(rowCount + ' rows returned');
+        });
+        connection.execSql(request);
+    }
